260135d618b6eae1e0627ca24da51192a2a8cc76
Jyotsna Pandey <jyotsnapandey6207@gmail.com>
2026-07-30 21:13:07 +0530

    Use Render ML recommender and remove .env from Git

--- routes/recommendRoutes.js
+++ routes/recommendRoutes.js
@@ -3,11 +3,14 @@ const router = express.Router();
 
 router.post('/', async (req, res, next) => {
   try {
-    const response = await fetch('http://127.0.0.1:5001/recommend', {
+    const response = await fetch('https://temple-recommender.onrender.com/recommend', {
       method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
+      headers: {
+        'Content-Type': 'application/json',
+      },
       body: JSON.stringify(req.body),
     });
+
     const data = await response.json();
     res.json(data);
   } catch (err) {
