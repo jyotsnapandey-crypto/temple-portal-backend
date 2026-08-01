13d23771fef73da4316c07815176e02d5549553d
Jyotsna Pandey <jyotsnapandey6207@gmail.com>
2026-08-01 12:45:01 +0530

    Connect ML recommender to Render

--- routes/recommendRoutes.js
+++ routes/recommendRoutes.js
@@ -5,12 +5,9 @@ router.post('/', async (req, res, next) => {
   try {
     const response = await fetch('https://temple-recommender.onrender.com/recommend', {
       method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(req.body),
     });
-
     const data = await response.json();
     res.json(data);
   } catch (err) {
